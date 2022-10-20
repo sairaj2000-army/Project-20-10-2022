1d2abbc7b82aba7ad896175791fa94202b635709
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-20 13:28:20 +0530

    Update network_and_firewall.tf

--- network_and_firewall.tf
+++ network_and_firewall.tf
@@ -17,15 +17,7 @@ module "network" {
       subnet_private_access = "true"
     },
   ]
-  secondary_ranges = {
-    "${var.subnet1_name}" : [
-      { range_name = "${var.pod_subnet1_name}", ip_cidr_range = "10.10.16.0/20" }, # 4094 usable ips for pods
-      { range_name = "${var.pod_svc_subnet1_name}", ip_cidr_range = "10.10.8.0/21" }    # 2046 usable ips for services
-    ],
-    "${var.subnet1_name}" : [
-      { range_name = "${var.pod_subnet2_name}", ip_cidr_range = "10.10.48.0/20" }, # 4094 usable ips for pods
-      { range_name = "${var.pod_svc_subnet2_name}", ip_cidr_range = "10.10.40.0/21" }   # 2046 usable ips for services
-    ]
+
   }
 
   firewall_rules = [{
