a20e20d8d833ce019279cc1d80db7d7217f86f52
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-20 15:41:20 +0530

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -1,12 +1,39 @@
-module "acm" {
-  source           = "terraform-google-modules/kubernetes-engine/google//modules/acm"
-  version          = "18.0"
-  project_id       = var.project_id
-  cluster_name     = var.cluster_name
-  location         = module.gke.location
-  cluster_endpoint = module.gke.endpoint
+resource "google_gke_hub_membership" "gke_membership" {
+  membership_id = "${var.project_id}-${var.cluster_name}"
+  endpoint {
+    gke_cluster {
+      resource_link = "//container.googleapis.com/${module.gke.cluster_id}"
+    }
+  }
+  authority {
+    issuer = "https://container.googleapis.com/v1/${module.gke.cluster_id}"
+  }
+  provider = google-beta
+  depends_on = [module.gke]
+}
 
-  sync_repo   = var.sync_repo
-  sync_branch = var.sync_branch
-  policy_dir  = var.policy_dir
+resource "google_gke_hub_feature_membership" "feature_member_gke" {
+  provider   = google-beta
+  location   = "global"
+  feature    = "configmanagement"
+  membership = google_gke_hub_membership.gke_membership.membership_id
+      
+  configmanagement {
+    version = "1.13.0"
+    config_sync {
+      source_format = "unstructured"
+      git {
+        sync_repo   = var.sync_repo
+        sync_branch = var.sync_branch
+        policy_dir  = var.policy_dir
+        secret_type = "none"
+      }
+    }
+    policy_controller {
+      enabled                    = true
+      template_library_installed = true
+      referential_rules_enabled  = true
+    }
+  }
+
 }
