fd87f2ed2a0555b369593db3159566e035d475cc
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-20 16:03:04 +0530

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -17,37 +17,17 @@ module "network" {
       { range_name = "${pod_svc_subnet_name}", ip_cidr_range = "10.10.8.0/21" }    # 2046 usable ips for services
     ],
   }
-
-
-}
-
-module "firewall_rules" {
-  source       = "terraform-google-modules/network/google//modules/firewall-rules"
-  project_id   = var.project_id
-  network_name = var.network_name
-
-  rules = [{
-    name      = "allow-ssh-ingress"
-    direction = "INGRESS"
-    ranges    = ["10.10.0.0/18"]
-    allow = [{
-      protocol = "tcp"
-      ports    = ["22"]
-    }]
-    }, {
-    name      = "Allow Pod to Pod connectivity"
-    direction = "INGRESS"
-    ranges    = ["10.10.0.0/18"]
+  
+  firewall_rules = [{
+    name        = var.firewall_name
+    description = "Allow Pod to Pod connectivity"
+    direction   = "INGRESS"
+    ranges      = ["10.10.0.0/18"]
     allow = [{
       protocol = "tcp"
       ports    = ["0-65535"]
     }]
-    }, {
-    name    = "allow-sidecar"
-    network = var.network_name
-    allow = [{
-      protocol = "tcp"
-      ports    = ["443", "15017"]
-    }]
   }]
+
+
 }
