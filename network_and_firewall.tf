1d63f09d19e4e0069456170a8c4d46a40c414544
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-20 12:05:51 +0530

    Update network_and_firewall.tf

--- network_and_firewall.tf
+++ network_and_firewall.tf
@@ -5,13 +5,13 @@ module "network" {
   project_id   = var.project_id #module.project.project_id
   subnets = [
     {
-      subnet_name           = {var.subnet1_name}
+      subnet_name           = "${var.subnet1_name}"
       subnet_ip             = "10.10.0.0/21" # 2046 usable ips for nodes
       subnet_region         = var.region1_name
       subnet_private_access = "true"
     },
     {
-      subnet_name           = {var.subnet2_name}
+      subnet_name           = "${var.subnet2_name}"
       subnet_ip             = "10.10.32.0/21" # 2046 usable ips for nodes
       subnet_region         = var.region1_name
       subnet_private_access = "true"
