cf1707dbf1b66dedb5436aa27354b37b140481b2
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-20 16:28:45 +0530

    Update acm_cluster2.tf

--- acm_cluster2.tf
+++ acm_cluster2.tf
@@ -1,7 +1,7 @@
 
 #register this cluster as a member of the fleet
 resource "google_gke_hub_membership" "cluster2_membership" {
-  membership_id = "${var.project_id}-${var.cluster2_name}"
+  membership_id = "${var.project_id}-new-${var.cluster2_name}"
   endpoint {
     gke_cluster {
       resource_link = "//container.googleapis.com/${module.cluster2.cluster_id}"
