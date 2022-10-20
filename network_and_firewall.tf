ad91565b1244e3c6dea8f7f78395c2ae9aeebf7c
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-20 12:36:26 +0530

    Update network_and_firewall.tf

--- network_and_firewall.tf
+++ network_and_firewall.tf
@@ -6,25 +6,25 @@ module "network" {
   subnets = [
     {
       subnet_name           = "${var.subnet1_name}"
-      subnet_ip             = "10.10.0.0/21" # 2046 usable ips for nodes
+      subnet_ip             = "20.10.0.0/21" # 2046 usable ips for nodes
       subnet_region         = var.region1_name
       subnet_private_access = "true"
     },
     {
       subnet_name           = "${var.subnet2_name}"
-      subnet_ip             = "10.10.32.0/21" # 2046 usable ips for nodes
+      subnet_ip             = "20.10.32.0/21" # 2046 usable ips for nodes
       subnet_region         = var.region1_name
       subnet_private_access = "true"
     },
   ]
   secondary_ranges = {
     "${var.subnet1_name}" : [
-      { range_name = "${var.pod_subnet1_name}", ip_cidr_range = "10.10.16.0/20" }, # 4094 usable ips for pods
-      { range_name = "${var.pod_svc_subnet1_name}", ip_cidr_range = "10.10.8.0/21" }    # 2046 usable ips for services
+      { range_name = "${var.pod_subnet1_name}", ip_cidr_range = "20.10.16.0/20" }, # 4094 usable ips for pods
+      { range_name = "${var.pod_svc_subnet1_name}", ip_cidr_range = "20.10.8.0/21" }    # 2046 usable ips for services
     ],
     "${var.subnet1_name}" : [
-      { range_name = "${var.pod_subnet2_name}", ip_cidr_range = "10.10.48.0/20" }, # 4094 usable ips for pods
-      { range_name = "${var.pod_svc_subnet2_name}", ip_cidr_range = "10.10.40.0/21" }   # 2046 usable ips for services
+      { range_name = "${var.pod_subnet2_name}", ip_cidr_range = "20.10.48.0/20" }, # 4094 usable ips for pods
+      { range_name = "${var.pod_svc_subnet2_name}", ip_cidr_range = "20.10.40.0/21" }   # 2046 usable ips for services
     ]
   }
 
@@ -32,7 +32,7 @@ module "network" {
     name        = var.firewall_name
     description = "Allow Pod to Pod connectivity"
     direction   = "INGRESS"
-    ranges      = ["10.10.0.0/18"]
+    ranges      = ["20.10.0.0/18"]
     allow = [{
       protocol = "tcp"
       ports    = ["0-65535"]
