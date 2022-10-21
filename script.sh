9334bd7ff26ec773b908adcd082189b600469323
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-21 07:44:21 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,18 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+data:
+  mesh: |-
+    defaultConfig:
+      tracing:
+        stackdriver: {}
+kind: ConfigMap
+metadata:
+  name: istio-asm-managed
+  namespace: istio-system
+EOF
+
+
+kubectl label namespace default istio.io/rev=asm-managed --overwrite
+
+kubectl annotate --overwrite namespace default \
+  mesh.cloud.google.com/proxy='{"managed":"true"}'
