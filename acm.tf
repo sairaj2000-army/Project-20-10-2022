6dd3f7c904083660936a5add8c4d0341c396d5c0
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-25 14:25:42 +0530

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -24,3 +24,30 @@ resource "google_gke_hub_feature_membership" "feature_member_gke" {
   }
   depends_on = [google_gke_hub_membership.gke_membership, null_resource.istio_ins, null_resource.script_for_istio, null_resource.getting_Cred, null_resource.apply_menifest, ]
 }
+
+
+resource "google_gke_hub_feature_membership" "feature_member_gke2" {
+  provider   = google-beta
+  location   = "global"
+  feature    = "configmanagement"
+  membership = google_gke_hub_membership.gke_membership.membership_id
+      
+  configmanagement {
+    version = "1.13.0"
+    config_sync {
+      source_format = "unstructured"
+      git {
+        sync_repo   = var.sync_repo2
+        sync_branch = var.sync_branch2
+        policy_dir  = var.policy_dir2
+        secret_type = "none"
+      }
+    }
+    policy_controller {
+      enabled                    = true
+      template_library_installed = true
+      referential_rules_enabled  = true
+    }
+  }
+  depends_on = [google_gke_hub_feature_membership.feature_member_gke]
+}
