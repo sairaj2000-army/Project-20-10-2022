561dfdf8c10ac3720c7ca84d96e719409fec7a26
Kumar Kartik <111974245+kumarkartik-deloitte@users.noreply.github.com>
2022-10-25 13:01:13 +0530

    Update membership.tf

--- membership.tf
+++ membership.tf
@@ -123,7 +123,7 @@ resource "null_resource" "apply_menifest" {
 
 # apply policies
 locals{
-  apply_policies = "kubectl apply -k policies/ "
+  apply_policies = "kubectl apply -R policies/ "
 }
 resource "null_resource" "apply_pol" {
   provisioner "local-exec" {
